9c8ec88994aa5b8246d6b9688a214230a5799282
heather <hjohnsick@yahoo.com>
2020-02-13 08:41:36 -0500

    AlligatorsAndSheepClasses

---
+++ AlligatorsAndSheep/Countable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlligatorsAndSheep
+{
+    interface Countable
+    {
+        void IncrementCount()
+        void ResetCount()
+        int GetCount()
+        String GetCountString()
+    }
+}
---
+++ AlligatorsAndSheep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlligatorsAndSheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
